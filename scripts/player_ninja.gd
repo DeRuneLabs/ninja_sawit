625180d3d72e315fd1eeab7d31c8ae5e27de7184
slowy07 <slowy.arfy@proton.me>
2025-03-01 19:23:56 +0700

    chore: create new prologue
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- scripts/player_ninja.gd
+++ scripts/player_ninja.gd
@@ -39,10 +39,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		print(velocity.y)
 		
 		
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
